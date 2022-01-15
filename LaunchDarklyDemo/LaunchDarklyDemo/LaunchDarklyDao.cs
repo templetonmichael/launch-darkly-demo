216e3774c8b130dcde2d483838e11605379a82e4
Michael Templeton <templetonmichael@live.com>
2022-01-15 10:14:18 -0600

    Initial Commit

---
+++ LaunchDarklyDemo/LaunchDarklyDemo/LaunchDarklyDao.cs
@@ -0,0 +1,25 @@
+﻿using LaunchDarkly.Sdk;
+using LaunchDarkly.Sdk.Client;
+
+namespace LaunchDarklyDemo
+{
+    public static class LaunchDarklyDao
+    {
+        private static LdClient? client;
+        private static User user = User.WithKey("valid_user");
+
+        public static async Task InitiateClient()
+        {
+            client = await LdClient.InitAsync("mob-0c2f306c-1842-474f-bbc2-930b5da6617c", user);
+        }
+        
+        public static bool? GetTestBool()
+        {
+            if(client != null)
+            {
+                return client.BoolVariation("test-bool", false);
+            }
+            return null;
+        }
+    }
+}
---
+++ LaunchDarklyDemo/LaunchDarklyDemo/Program.cs
@@ -0,0 +1,7 @@
+﻿using LaunchDarkly.Sdk;
+using LaunchDarkly.Sdk.Client;
+using LaunchDarklyDemo;
+
+await LaunchDarklyDao.InitiateClient();
+
+Console.WriteLine($"\nLaunch Darkly Feature Flag Result: {LaunchDarklyDao.GetTestBool()}");
